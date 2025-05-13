7ae925755219da0eaf525657571c7548239d74f2
vishnuwebdev2425 <akkupallivishnu2424@gmail.com>
2025-05-13 07:07:13 +0530

    intialcommit

---
+++ 9.js
@@ -0,0 +1,6 @@
+let values=document.getElementById("userdetails")
+function buttonenter(){
+    let intial_value=values.value;
+    let final_value="Hi welcome "
+    document.getElementById("show").textContent=final_value+intial_value;
+}
